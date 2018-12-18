baca692cc23a1980abca79795107d6e0e3b1fd03
Григорий Ким <kim-g@yandex.ru>
2018-12-18 15:29:44 +0500

    Общий проект БД

---
+++ MoleculeDataBase/AES_Data.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace MoleculeDataBase
+{
+    [Serializable]
+    public class AES_Data : Serializable
+    {
+        public byte[] AesIV;
+        public byte[] AesKey;
+
+
+        public AES_Data()
+        {
+        }
+
+        public void CreateData()
+        {
+            RNGCryptoServiceProvider r = new RNGCryptoServiceProvider();
+            AesKey = new byte[0x20];
+            AesIV = new byte[0x10];
+            r.GetNonZeroBytes(AesKey);
+            r.GetNonZeroBytes(AesIV);
+        }
+
+        /// <summary>
+        /// Создание нового блока AES с новым вектором шифрования.
+        /// </summary>
+        /// <returns></returns>
+        public static AES_Data NewAES()
+        {
+            AES_Data New = new AES_Data();
+            New.CreateData();
+            return New;
+        }
+
+        // Щифрует набор байт, используя текущие ключ и IV
+        public byte[] EncryptBytes(byte[] Info)
+        {
+            // Проверка аргументов
+            if (Info == null || Info.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (AesKey == null || AesKey.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (AesIV == null || AesIV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Создаем объект класса AES
+            // с определенным ключом and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = AesKey;
+                aesAlg.IV = AesIV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для шифрования.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(Info, 0, Info.Length);
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            //Возвращаем зашифрованные байты из потока памяти.
+            return encrypted;
+        }
+
+
+        // Шифрует строку, используя текущие ключ и IV
+        public byte[] EncryptStringToBytes(string plainText)
+        {
+            // Проверка аргументов
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (AesKey == null || AesKey.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (AesIV == null || AesIV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Создаем объект класса AES
+            // с определенным ключом and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = AesKey;
+                aesAlg.IV = AesIV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для шифрования.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Записываем в поток все данные.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            //Возвращаем зашифрованные байты из потока памяти.
+            return encrypted;
+
+        }
+
+        // Дешифрует массив байт, используя текущие ключ и IV
+        public byte[] DecryptBytes(byte[] cipherText)
+        {
+            // Проверяем аргументы
+            if (cipherText == null || cipherText.Length <= 0)
+            {
+                return null;
+            };
+            if (AesKey == null || AesKey.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (AesIV == null || AesIV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Строка, для хранения расшифрованного текста
+            byte[] Info;
+
+            // Создаем объект класса AES,
+            // Ключ и IV
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = AesKey;
+                aesAlg.IV = AesIV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для расшифрования.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+
+                        Info = new byte[csDecrypt.Length];
+                        csDecrypt.Read(Info, 0, Convert.ToInt32(csDecrypt.Length));
+                    }
+                }
+            }
+
+            return Info;
+
+        }
+
+        // Дешифрует строку, используя текущие ключ и IV
+        public string DecryptStringFromBytes(byte[] cipherText)
+        {
+            /*string plaintext = Encoding.UTF8.GetString(DecryptBytes(cipherText));
+
+            return plaintext;*/
+
+            // Проверяем аргументы
+            if (cipherText == null || cipherText.Length <= 0)
+            {
+                return "";
+            };
+            if (AesKey == null || AesKey.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (AesIV == null || AesIV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Строка, для хранения расшифрованного текста
+            string plaintext;
+
+            // Создаем объект класса AES,
+            // Ключ и IV
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = AesKey;
+                aesAlg.IV = AesIV;
+
+                // Создаем объект, который определяет основные операции преобразований.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создаем поток для расшифрования.
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Читаем расшифрованное сообщение и записываем в строку
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+    }
+}
---
+++ MoleculeDataBase/Serializable.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MoleculeDataBase
+{
+    [Serializable]
+    public class Serializable
+    {
+        public void SaveToFile(string FileName)
+        {
+            using (FileStream fs = File.OpenWrite(FileName))
+            {
+                MemoryStream ms = ToBin();
+                ms.CopyTo(fs);
+            }
+        }
+
+        /// <summary>
+        /// Сериализация в битовый формат
+        /// </summary>
+        /// <returns></returns>
+        public MemoryStream ToBin()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, this);
+            ms.Position = 0;
+            return ms;
+        }
+
+        /// <summary>
+        /// Сериализация в SOAP формат
+        /// </summary>
+        /// <returns></returns>
+        public string ToSOAP()
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, this);
+            ms.Position = 0;
+            byte[] ResByte = new byte[ms.Length];
+            ms.Read(ResByte, 0, ResByte.Length);
+            return Encoding.UTF8.GetString(ResByte);
+        }
+
+        /// <summary>
+        /// Сериализация в XML поток
+        /// </summary>
+        /// <returns></returns>
+        public MemoryStream ToXMLStream()
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            MemoryStream ms = new MemoryStream();
+            serializer.Serialize(ms, this);
+            ms.Position = 0;
+            return ms;
+        }
+
+        /// <summary>
+        /// Сериализация в XML
+        /// </summary>
+        /// <returns></returns>
+        public string ToXML()
+        {
+            MemoryStream ms = ToXMLStream();
+            byte[] buff = new byte[ms.Length];
+            ms.Read(buff, 0, buff.Length);
+            return Encoding.UTF8.GetString(buff);
+        }
+
+        /// <summary>
+        /// Десериализация из битового формата
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <returns></returns>
+        public static object FromBin(Stream ms)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            ms.Position = 0;            
+            return formatter.Deserialize(ms);
+        }
+
+        /// <summary>
+        /// Десериализация из SOAP формата
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <returns></returns>
+        public static Serializable FromSOAP(Stream ms)
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            ms.Position = 0;
+            return (Serializable)formatter.Deserialize(ms);
+        }
+
+        /// <summary>
+        /// Десериализация из SOAP формата
+        /// </summary>
+        /// <param name="SOAP"></param>
+        /// <returns></returns>
+        public static Serializable FromSOAP(string SOAP)
+        {
+            return FromSOAP(new MemoryStream(Encoding.UTF8.GetBytes(SOAP)));
+        }
+
+        /// <summary>
+        /// Десериализация из XML
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <returns></returns>
+        public static T From_XML<T>(Stream ms)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            ms.Position = 0;
+            return (T)serializer.Deserialize(ms);
+        }
+
+        public static T From_XML<T>(string Data)
+        {
+            return From_XML<T>(new MemoryStream(Encoding.UTF8.GetBytes(Data)));
+        }
+
+        static public Serializable LoadFromFile(string FileName)
+        {
+            using (FileStream fs = File.OpenRead(FileName))
+            {
+                return (Serializable)FromBin(fs);
+            }
+        }
+
+        static public T LoadFromXMLFile<T>(string FileName)
+        {
+            using (FileStream fs = File.OpenRead(FileName))
+            {
+                return From_XML<T>(fs);
+            }
+        }
+    }
+}
